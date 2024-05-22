fa788c469f0a0d8cff1488952a59a21fd3a2bb33
gittryout <kckprasad1984@gmail.com>
2024-05-22 10:09:57 +0530

    ccode-Firstcommit

---
+++ src/main/java/com/learner/ccode/CcodeApplication.java
@@ -0,0 +1,13 @@
+package com.learner.ccode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/ccode/CcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.ccode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
